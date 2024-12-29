3ed56697f930f01b5db99568191efa4b16e5b44b
Pranavbc <pranav1952000@gmail.com>
2024-12-29 21:42:04 +0530

    d

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,23 +1,50 @@
+// import React from "react";
+// import {Outlet} from "react-router-dom"
+// import Navbar from  "./components/Header/Navbar.jsx"
+// import Sidebar from "./components/Header/Sidebar.jsx";
+
+// function Layout (){
+
+//     return (
+//              <>
+//                   <Navbar />
+//                       <div className="sm:flex flex-none">
+//                       <div className="">
+//                            <Sidebar />
+//                       </div>
+//                            <div className="sm:flex-1">
+//                                <Outlet />
+//                            </div>
+//                      </div>
+//              </>
+//     )
+// }
+
+// export default Layout ;
+
+
 import React from "react";
-import {Outlet} from "react-router-dom"
-import Navbar from  "./components/Header/Navbar.jsx"
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Header/Navbar.jsx";
 import Sidebar from "./components/Header/Sidebar.jsx";
 
-function Layout (){
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        {/* Sidebar - visible on larger screens */}
+        <div className="hidden sm:block sm:w-64">
+          <Sidebar />
+        </div>
 
-    return (
-             <>
-                  <Navbar />
-                      <div className="sm:flex flex-none">
-                      <div className="">
-                           <Sidebar />
-                      </div>
-                           <div className="sm:flex-1">
-                               <Outlet />
-                           </div>
-                     </div>
-             </>
-    )
+        {/* Main content - takes remaining space */}
+        <div className="flex-1">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Layout ;
+export default Layout;
