7d6cc5d9a0d57e2a0707bbf4b19cadfe99f2404c
Pranavbc <pranav1952000@gmail.com>
2024-12-29 18:05:40 +0530

    g

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'https://vt-backend.onrender.com'
+//     }
+//   }
+// })
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,7 +20,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://vt-backend.onrender.com'
+      '/api': {
+        target: 'https://vt-backend.onrender.com',
+        changeOrigin: true, // Ensures that the Origin header is set correctly
+        secure: true, // Set to false if your backend is using self-signed certificates
+        rewrite: (path) => path.replace(/^\/api/, '') // Strips "/api" from the request path
+      }
     }
   }
 })
