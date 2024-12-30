5dcc2206d694f3ee546edadec57666aa8e702378
Pranavbc <pranav1952000@gmail.com>
2024-12-30 18:00:29 +0530

    chnage view for subscribers

--- src/pages/Channel/ChannelSubscribers.jsx
+++ src/pages/Channel/ChannelSubscribers.jsx
@@ -64,7 +64,7 @@ function ChannelSubscribers() {
                             }
                         >
                             {subscriber?.subscriber?.subscribedToSubscriber
-                                ? "Subscribed"
+                                ? "UnSubscribe"
                                 : "Subscribe"}
                         </Button>
                     </div>
