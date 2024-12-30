aca6e1b029d8de7ec8e6d210c0056067646ab64f
Pranavbc <pranav1952000@gmail.com>
2024-12-30 16:08:18 +0530

    k

--- vite.config.js
+++ vite.config.js
@@ -12,20 +12,28 @@
 // })
 
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://vt-backend.onrender.com/api/v1',
+//         // changeOrigin: true, // Ensures that the Origin header is set correctly
+//         // secure: true, // Set to false if your backend is using self-signed certificates
+//         // rewrite: (path) => path.replace(/^\/api/, '') // Strips "/api" from the request path
+//       }
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: './', // Use relative paths for assets
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://vt-backend.onrender.com/api/v1',
-        // changeOrigin: true, // Ensures that the Origin header is set correctly
-        // secure: true, // Set to false if your backend is using self-signed certificates
-        // rewrite: (path) => path.replace(/^\/api/, '') // Strips "/api" from the request path
-      }
-    }
-  }
-})
+});
