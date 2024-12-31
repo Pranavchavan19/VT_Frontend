637f9929ef0f9ce3eca3a73ed11fe0473da0fbcb
Pranavbc <pranav1952000@gmail.com>
2024-12-31 17:07:10 +0530

    mmmmmmmmmmmmm

---
+++ public/js/scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    // Hide the loading spinner once the page is fully loaded
+    document.getElementById('loading').style.display = 'none';
+  });
+  
