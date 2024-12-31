7599959850249841decc9788c6b866ebe80e1972
Pranavbc <pranav1952000@gmail.com>
2024-12-31 15:26:36 +0530

    adding cover image popup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Logo , Button ,Input} from "./index.js"
+import React, { useState } from "react";
+import { Logo, Button, Input } from "./index.js";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { createAccount, userLogin } from "../Store/Slices/authSlice.js";
@@ -14,12 +14,30 @@ function SignUp() {
         register,
         control,
         formState: { errors },
+        setError,
+        clearErrors,
     } = useForm();
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const loading = useSelector((state) => state.auth?.loading);
 
     const submit = async (data) => {
+        // If coverImage is required but not provided
+        if (!data.coverImage) {
+            setError("coverImage", { message: "Cover image is required" });
+            return;
+        } else {
+            clearErrors("coverImage");
+        }
+
+        // If avatar is required but not provided
+        if (!data.avatar) {
+            setError("avatar", { message: "Avatar is required" });
+            return;
+        } else {
+            clearErrors("avatar");
+        }
+
         const response = await dispatch(createAccount(data));
         if (response?.payload?.success) {
             const username = data?.username;
@@ -41,163 +59,125 @@ function SignUp() {
     }
 
     return (
-        <>
-            <div className="w-full h-screen bg-black text-white p-3 flex justify-center items-start sm:mt-8">
-                <div className="flex flex-col space-y-2 justify-center items-center border border-slate-600 p-3">
-                    <div className="flex items-center gap-2">
-                        <Logo />
-                    </div>
-                    <form
-                        onSubmit={handleSubmit(submit)}
-                        className="space-y-4 p-2 text-sm sm:w-96 w-full"
-                    >
-                        <div className="w-full relative h-28 bg-[#222222]">
-                            <div className="w-full h-full">
-                                <GetImagePreview
-                                    name="coverImage"
-                                    control={control}
-                                    className="w-full h-28 object-cover border-none border-slate-900"
-                                    cameraIcon
-                                />
-                                <div className="text-sm absolute right-2 bottom-2 hover:text-purple-500 cursor-default">
-                                    cover Image
-                                </div>
-                            </div>
-                            <div className="absolute left-2 bottom-2 rounded-full border-2">
-                                <GetImagePreview
-                                    name="avatar"
-                                    control={control}
-                                    className="object-cover rounded-full h-20 w-20 outline-none"
-                                    cameraIcon={true}
-                                    cameraSize={20}
-                                />
-                            </div>
-
-                            {/* <label
-                                htmlFor="avatar"
-                                className="cursor-pointer"
-                            >
-                                <div className="absolute h-24 w-24 left-2 bottom-2 flex justify-center items-center">
-                                    <img
-                                        src={avatarPreview}
-                                        className=" object-cover w-full h-full border-2 border-double rounded-full"
-                                    />
-                                    <FaCamera
-                                        className="absolute hover:text-purple-500"
-                                        size={20}
-                                    />
-                                </div>
-                                <Controller
-                                    name="avatar"
-                                    control={control}
-                                    render={({ field: { onChange } }) => (
-                                        <input
-                                            id="avatar"
-                                            type="file"
-                                            className="hidden"
-                                            accept="image/png, image/jpeg"
-                                            onChange={(e) => {
-                                                onChange(handleAvatarChange(e));
-                                            }}
-                                        />
-                                    )}
-                                    rules={{ required: "avatar is required" }}
-                                />
-                            </label> */}
-                        </div>
-                        {errors.avatar && (
-                            <div className="text-red-500">
-                                {errors.avatar.message}
-                            </div>
-                        )}
-                        <Input
-                            label="Username: "
-                            type="text"
-                            placeholder="Enter username"
-                            {...register("username", {
-                                required: "username is required",
-                            })}
-                            className="h-8"
-                        />
-                        {errors.username && (
-                            <span className="text-red-500">
-                                {errors.username.message}
-                            </span>
-                        )}
-                        <Input
-                            label="Email: "
-                            type="email"
-                            placeholder="Enter email"
-                            {...register("email", {
-                                required: "email is required",
-                            })}
-                            className="h-8"
-                        />
-                        {errors.email && (
-                            <span className="text-red-500">
-                                {errors.email.message}
-                            </span>
-                        )}
-                        <Input
-                            label="Fullname: "
-                            type="text"
-                            placeholder="Enter fullname"
-                            {...register("fullName", {
-                                required: "fullName is required",
-                            })}
-                            className="h-8"
-                        />
-                        {errors.fullName && (
-                            <span className="text-red-500">
-                                {errors.fullName.message}
-                            </span>
-                        )}
-                        <Input
-                            label="Password: "
-                            type="password"
-                            placeholder="Enter password"
-                            {...register("password", {
-                                required: "password is required",
-                            })}
-                            className="h-8"
-                        />
-                        {errors.password && (
-                            <span className="text-red-500">
-                                {errors.password.message}
-                            </span>
-                        )}
-
-                        <Button
-                            type="submit"
-                            bgColor="bg-purple-500"
-                            className="w-full sm:py-3 py-2 hover:bg-purple-700 text-lg"
-                        >
-                            Signup
-                        </Button>
-
-                        <p className="text-center text-sm">
-                            Already have an account?{" "}
-                            <Link
-                                to={"/login"}
-                                className="text-purple-600 cursor-pointer hover:opacity-70"
-                            >
-                                Login
-                            </Link>
-                        </p>
-                    </form>
+        <div className="w-full h-screen bg-black text-white p-3 flex justify-center items-start sm:mt-8">
+            <div className="flex flex-col space-y-2 justify-center items-center border border-slate-600 p-3">
+                <div className="flex items-center gap-2">
+                    <Logo />
                 </div>
+                <form
+                    onSubmit={handleSubmit(submit)}
+                    className="space-y-4 p-2 text-sm sm:w-96 w-full"
+                >
+                    <div className="w-full relative h-28 bg-[#222222]">
+                        <div className="w-full h-full">
+                            <GetImagePreview
+                                name="coverImage"
+                                control={control}
+                                className="w-full h-28 object-cover border-none border-slate-900"
+                                cameraIcon
+                            />
+                            <div className="text-sm absolute right-2 bottom-2 hover:text-purple-500 cursor-default font-bold ">
+                                Cover Image
+                            </div>
+                        </div>
+                        <div className="absolute left-2 bottom-2 rounded-full border-2">
+                            <GetImagePreview
+                                name="avatar"
+                                control={control}
+                                className="object-cover rounded-full h-20 w-20 outline-none"
+                                cameraIcon={true}
+                                cameraSize={20}
+                            />
+                        </div>
+                    </div>
+                    {errors.coverImage && (
+                        <div className="text-red-500 font-bold text-lg capitalize">
+                            {errors.coverImage.message}
+                        </div>
+                    )}
+                    {errors.avatar && (
+                        <div className="text-red-500 font-bold text-lg capitalize">
+                            {errors.avatar.message}
+                        </div>
+                    )}
+                    <Input
+                        label="Username: "
+                        type="text"
+                        placeholder="Enter username"
+                        {...register("username", {
+                            required: "username is required",
+                        })}
+                        className="h-8"
+                    />
+                    {errors.username && (
+                        <span className="text-red-500">
+                            {errors.username.message}
+                        </span>
+                    )}
+                    <Input
+                        label="Email: "
+                        type="email"
+                        placeholder="Enter email"
+                        {...register("email", {
+                            required: "email is required",
+                        })}
+                        className="h-8"
+                    />
+                    {errors.email && (
+                        <span className="text-red-500">
+                            {errors.email.message}
+                        </span>
+                    )}
+                    <Input
+                        label="Fullname: "
+                        type="text"
+                        placeholder="Enter fullname"
+                        {...register("fullName", {
+                            required: "fullName is required",
+                        })}
+                        className="h-8"
+                    />
+                    {errors.fullName && (
+                        <span className="text-red-500">
+                            {errors.fullName.message}
+                        </span>
+                    )}
+                    <Input
+                        label="Password: "
+                        type="password"
+                        placeholder="Enter password"
+                        {...register("password", {
+                            required: "password is required",
+                        })}
+                        className="h-8"
+                    />
+                    {errors.password && (
+                        <span className="text-red-500">
+                            {errors.password.message}
+                        </span>
+                    )}
+
+                    <Button
+                        type="submit"
+                        bgColor="bg-purple-500"
+                        className="w-full sm:py-3 py-2 hover:bg-purple-700 text-lg"
+                    >
+                        Signup
+                    </Button>
+
+                    <p className="text-center text-sm">
+                        Already have an account?{" "}
+                        <Link
+                            to={"/login"}
+                            className="text-purple-600 cursor-pointer hover:opacity-70"
+                        >
+                            Login
+                        </Link>
+                    </p>
+                </form>
             </div>
-        </>
+        </div>
     );
 }
 
 export default SignUp;
-
-
-
-
-
-
-
-
-
