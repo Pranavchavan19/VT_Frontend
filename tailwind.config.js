56d4c999078162e6b5ecf2b0cb83ac05d9eeeafc
Pranavbc <pranav1952000@gmail.com>
2024-12-31 12:32:21 +0530

    bb

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,24 @@
 //   plugins: [],
 // }
 
-export default {
-  content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/index.html",
-  ],
-  theme: {
-      extend: {},
-  },
-  plugins: [],
-};
-
-
-// module.exports = {
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+// export default {
+//   content: [
+//       "./src/**/*.{js,jsx,ts,tsx}",
+//       "./public/index.html",
+//   ],
 //   theme: {
 //       extend: {},
 //   },
 //   plugins: [],
 // };
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
