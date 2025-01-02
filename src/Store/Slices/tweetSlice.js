1d294a8adc59fae23a6a0731d6ac166f2c3effb3
Pranavbc <pranav1952000@gmail.com>
2025-01-02 17:50:36 +0530

    add for likes count

--- src/Store/Slices/tweetSlice.js
+++ src/Store/Slices/tweetSlice.js
@@ -1,5 +1,111 @@
 
 
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axiosInstance from "../../helpers/axiosInstance";
+// import toast from "react-hot-toast";
+
+// const initialState = {
+//     loading: false,
+//     tweets: [],
+// };
+
+// export const createTweet = createAsyncThunk("createTweet", async (content) => {
+//     try {
+//         const response = await axiosInstance.post("/tweet", content);
+//         toast.success(response.data?.message);
+//         return response.data.data;
+//     } catch (error) {
+//         toast.error(error?.response?.data?.error);
+//         throw error;
+//     }
+// });
+
+// export const editTweet = createAsyncThunk(
+//     "editTweet",
+//     async ({ tweetId, content }) => {
+//         try {
+//             const response = await axiosInstance.patch(
+//                 `/tweet/${tweetId}`,
+//                 {content}
+//             );
+//             toast.success(response.data.message);
+//             return response.data.data;
+//         } catch (error) {
+//             toast.error(error?.response?.data?.error);
+//             throw error;
+//         }
+//     }
+// );
+
+// export const deleteTweet = createAsyncThunk("deleteTweet", async (tweetId) => {
+//     try {
+//         const response = await axiosInstance.delete(`/tweet/${tweetId}`);
+//         toast.success(response.data.message);
+//         return response.data.data.tweetId;
+//     } catch (error) {
+//         toast.error(error?.response?.data?.error);
+//         throw error;
+//     }
+// });
+
+// export const getUserTweets = createAsyncThunk( "getUserTweets", async (userId) => {
+//         try {
+//             const response = await axiosInstance.get(`/tweet/user/${userId}`);
+//             return response.data.data;
+//         } catch (error) {
+//             toast.error(error?.response?.data?.error);
+//             throw error;
+//         }
+//     }
+// );
+
+// export const getAllTweets = createAsyncThunk("getAllTweets", async () => {
+//     try {
+//         const response = await axiosInstance.get("/tweet"); // Public API endpoint
+//         return response.data.data;
+//     } catch (error) {
+//         toast.error(error?.response?.data?.error);
+//         throw error;
+//     }
+// });
+
+
+// const tweetSlice = createSlice({
+//     name: "tweet",
+//     initialState,
+//     reducers: {},
+//     extraReducers: (builder) => {
+//         builder.addCase( getUserTweets.pending, (state) => {
+//             state.loading = true;
+//         }
+//         );
+//         builder.addCase(getUserTweets.fulfilled, (state, action) => {
+//             state.loading = false;
+//             state.tweets = action.payload;
+//         });
+//         builder.addCase(createTweet.fulfilled, (state, action) => {
+//             state.tweets.unshift(action.payload);
+//         })
+//         builder.addCase(deleteTweet.fulfilled, (state, action) => {
+//             state.tweets = state.tweets.filter((tweet) => tweet._id !== action.payload);
+//         })
+
+//         builder.addCase(getAllTweets.pending, (state) => {
+//             state.loading = true;
+//         });
+//         builder.addCase(getAllTweets.fulfilled, (state, action) => {
+//             state.loading = false;
+//             state.tweets = action.payload;
+//         });
+        
+//     },
+// });
+
+// export const {addTweet} = tweetSlice.actions;
+
+// export default tweetSlice.reducer;
+
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../helpers/axiosInstance";
 import toast from "react-hot-toast";
@@ -9,6 +115,7 @@ const initialState = {
     tweets: [],
 };
 
+// Async Thunks
 export const createTweet = createAsyncThunk("createTweet", async (content) => {
     try {
         const response = await axiosInstance.post("/tweet", content);
@@ -20,48 +127,10 @@ export const createTweet = createAsyncThunk("createTweet", async (content) => {
     }
 });
 
-export const editTweet = createAsyncThunk(
-    "editTweet",
-    async ({ tweetId, content }) => {
-        try {
-            const response = await axiosInstance.patch(
-                `/tweet/${tweetId}`,
-                {content}
-            );
-            toast.success(response.data.message);
-            return response.data.data;
-        } catch (error) {
-            toast.error(error?.response?.data?.error);
-            throw error;
-        }
-    }
-);
-
-export const deleteTweet = createAsyncThunk("deleteTweet", async (tweetId) => {
+export const editTweet = createAsyncThunk("editTweet", async ({ tweetId, content }) => {
     try {
-        const response = await axiosInstance.delete(`/tweet/${tweetId}`);
+        const response = await axiosInstance.patch(`/tweet/${tweetId}`, { content });
         toast.success(response.data.message);
-        return response.data.data.tweetId;
-    } catch (error) {
-        toast.error(error?.response?.data?.error);
-        throw error;
-    }
-});
-
-export const getUserTweets = createAsyncThunk( "getUserTweets", async (userId) => {
-        try {
-            const response = await axiosInstance.get(`/tweet/user/${userId}`);
-            return response.data.data;
-        } catch (error) {
-            toast.error(error?.response?.data?.error);
-            throw error;
-        }
-    }
-);
-
-export const getAllTweets = createAsyncThunk("getAllTweets", async () => {
-    try {
-        const response = await axiosInstance.get("/tweet"); // Public API endpoint
         return response.data.data;
     } catch (error) {
         toast.error(error?.response?.data?.error);
@@ -69,26 +138,76 @@ export const getAllTweets = createAsyncThunk("getAllTweets", async () => {
     }
 });
 
+export const deleteTweet = createAsyncThunk("deleteTweet", async (tweetId) => {
+    try {
+        const response = await axiosInstance.delete(`/tweet/${tweetId}`);
+        toast.success(response.data.message);
+        return tweetId;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error;
+    }
+});
 
+export const getUserTweets = createAsyncThunk("getUserTweets", async (userId) => {
+    try {
+        const response = await axiosInstance.get(`/tweet/user/${userId}`);
+        return response.data.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error;
+    }
+});
+
+export const getAllTweets = createAsyncThunk("getAllTweets", async () => {
+    try {
+        const response = await axiosInstance.get("/tweet");
+        return response.data.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error;
+    }
+});
+
+export const likeTweet = createAsyncThunk("likeTweet", async (tweetId) => {
+    try {
+        const response = await axiosInstance.post(`/tweet/${tweetId}/like`);
+        return { tweetId, likesCount: response.data.likesCount, isLiked: response.data.isLiked };
+    } catch (error) {
+        toast.error(error.response?.data?.error || "Failed to like tweet");
+        throw error;
+    }
+});
+
+// Slice
 const tweetSlice = createSlice({
     name: "tweet",
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase( getUserTweets.pending, (state) => {
+        builder.addCase(getUserTweets.pending, (state) => {
             state.loading = true;
-        }
-        );
+        });
         builder.addCase(getUserTweets.fulfilled, (state, action) => {
             state.loading = false;
             state.tweets = action.payload;
         });
+
         builder.addCase(createTweet.fulfilled, (state, action) => {
             state.tweets.unshift(action.payload);
-        })
+        });
+
+        builder.addCase(editTweet.fulfilled, (state, action) => {
+            const updatedTweet = action.payload;
+            const index = state.tweets.findIndex((tweet) => tweet._id === updatedTweet._id);
+            if (index !== -1) {
+                state.tweets[index] = updatedTweet;
+            }
+        });
+
         builder.addCase(deleteTweet.fulfilled, (state, action) => {
             state.tweets = state.tweets.filter((tweet) => tweet._id !== action.payload);
-        })
+        });
 
         builder.addCase(getAllTweets.pending, (state) => {
             state.loading = true;
@@ -97,10 +216,16 @@ const tweetSlice = createSlice({
             state.loading = false;
             state.tweets = action.payload;
         });
-        
+
+        builder.addCase(likeTweet.fulfilled, (state, action) => {
+            const { tweetId, likesCount, isLiked } = action.payload;
+            const tweet = state.tweets.find((t) => t._id === tweetId);
+            if (tweet) {
+                tweet.likesCount = likesCount;
+                tweet.isLiked = isLiked;
+            }
+        });
     },
 });
-
-export const {addTweet} = tweetSlice.actions;
 
 export default tweetSlice.reducer;
--- src/pages/TweetsFeed.jsx
+++ src/pages/TweetsFeed.jsx
@@ -1,187 +1,5 @@
 
 
-
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { timeAgo } from "../helpers/timeAgo";  // Helper for time formatting
-import { Like, DeleteConfirmation, Edit } from "../components/index";
-import { FaTwitter } from "../components/icons";
-import { getAllTweets, deleteTweet, editTweet } from "../Store/Slices/tweetSlice";
-
-function TweetsFeed() {
-    const avatar2 = useSelector((state) => state.user?.profileData?.avatar?.url); // Default avatar
-    const authUsername = useSelector((state) => state.auth?.userData?.username); // Current user’s username
-    const tweets = useSelector((state) => state.tweet?.tweets); // Fetch tweets from state
-    const dispatch = useDispatch();
-
-    // Track which tweet is being edited or deleted
-    const [editState, setEditState] = useState({
-        editingTweetId: null, // Track tweet being edited
-        editedContent: "",     // Edited tweet content
-        deleteTweetId: null,   // Track tweet being deleted
-        isOpen: false,         // Flag for showing options
-    });
-
-    // Fetch all tweets when the component mounts
-    useEffect(() => {
-        dispatch(getAllTweets());  // Fetch tweets from the store
-    }, [dispatch]);
-
-    const handleEditTweet = (tweetId, editedContent) => {
-        // Dispatch edit action to the store
-        dispatch(editTweet({ tweetId, content: editedContent }));
-        setEditState({
-            ...editState,
-            editingTweetId: null,
-            editedContent: "",
-            isOpen: false,
-        });
-    };
-
-    const handleDeleteTweet = (tweetId) => {
-        // Dispatch delete action to the store
-        dispatch(deleteTweet(tweetId));
-        setEditState({
-            ...editState,
-            deleteTweetId: null,
-            isOpen: false,
-        });
-    };
-
-    // Function to handle opening edit/delete options
-    const handleTweetOptions = (tweetId, content) => {
-        setEditState({
-            ...editState,
-            isOpen: !editState.isOpen,
-            editingTweetId: tweetId, // Set the tweet being edited
-            editedContent: content,  // Prepopulate content for editing
-        });
-    };
-
-    return (
-        <div className="text-white w-full flex flex-col gap-3">
-            {tweets.map((tweet) => (
-                <div key={tweet._id} className="w-full flex justify-start items-center sm:gap-5 gap-3 border-b border-slate-600 p-3 sm:p-5">
-                    <div className="w-10">
-                        <img
-                            src={tweet.owner?.avatar?.url || avatar2}  // Use tweet owner’s avatar or fallback to default
-                            className="w-8 h-8 object-cover rounded-full"
-                            alt="Avatar"
-                        />
-                    </div>
-                    <div className="w-full flex flex-col gap-1 relative">
-                        <div className="flex items-center gap-2">
-                            <h2 className="text-xs">{tweet.owner?.username || "Anonymous"}</h2> {/* Display username or anonymous */}
-                            <span className="text-xs text-slate-400">
-                                {timeAgo(tweet.createdAt)} {/* Display tweet creation time */}
-                            </span>
-                        </div>
-
-                        {/* Display tweet content or edit form
-                        {editState.editingTweetId === tweet._id ? (
-                            <Edit
-                                initialContent={editState.editedContent}
-                                onCancel={() =>
-                                    setEditState({ ...editState, editingTweetId: null, isOpen: false })
-                                }
-                                onSave={(editedContent) => handleEditTweet(tweet._id, editedContent)}
-                            />
-                        ) : (
-                            <p>{tweet.content}</p>
-                        )} */}
-
-                         {editState.editing ? (
-                           <Edit
-                                 initialContent={editState.editedContent}
-                                 onCancel={() =>
-                                 setEditState((prevState) => ({
-                                 ...prevState,
-                                 editing: false,
-                                 isOpen: false,
-                                 }))
-                                 }
-                                  onSave={handleEditTweet}
-                            />
-                            ) : (
-                            <p>{editState.editedContent}</p>
-                            )}
-
-                        {/* Like the tweet */}
-                        <Like
-                            isLiked={tweet.isLiked}
-                            likesCount={tweet.likesCount}
-                            tweetId={tweet._id}
-                            size={20}
-                        />
-
-                        {/* Edit/Delete Options (only show if the tweet belongs to the logged-in user) */}
-                        {authUsername === tweet.owner?.username && (
-                            <>
-                                <div
-                                    className="w-5 h-5 absolute right-0 cursor-pointer"
-                                    onClick={() => handleTweetOptions(tweet._id, tweet.content)}
-                                >
-                                    <FaTwitter />
-                                </div>
-
-                                {editState.isOpen && editState.editingTweetId === tweet._id && (
-                                    <div className="border bg-[#222222] text-lg border-slate-600 absolute text-center right-5 rounded-xl">
-                                        <ul>
-                                            <li
-                                                className="hover:opacity-50 px-5 cursor-pointer border-b border-slate-600"
-                                                onClick={() =>
-                                                    setEditState({
-                                                        ...editState,
-                                                        editingTweetId: tweet._id,
-                                                        isOpen: false,
-                                                    })
-                                                }
-                                            >
-                                                Edit
-                                            </li>
-                                            <li
-                                                className="px-5 hover:opacity-50 cursor-pointer"
-                                                onClick={() =>
-                                                    setEditState({
-                                                        ...editState,
-                                                        deleteTweetId: tweet._id,
-                                                        isOpen: false,
-                                                    })
-                                                }
-                                            >
-                                                Delete
-                                            </li>
-                                        </ul>
-                                    </div>
-                                )}
-                            </>
-                        )}
-
-                        {/* Delete Confirmation */}
-                        {editState.deleteTweetId === tweet._id && (
-                            <DeleteConfirmation
-                                tweet={true}
-                                onCancel={() =>
-                                    setEditState({ ...editState, deleteTweetId: null })
-                                }
-                                onDelete={() => handleDeleteTweet(tweet._id)}
-                            />
-                        )}
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-}
-
-export default TweetsFeed;
-
-
-
-
-
-
-
 // import React, { useState, useEffect } from "react";
 // import { useSelector, useDispatch } from "react-redux";
 // import { timeAgo } from "../helpers/timeAgo";  // Helper for time formatting
@@ -197,7 +15,7 @@ export default TweetsFeed;
 //     content,
 //     likesCount = 0,
 //     isLiked,
-// }){
+// })  {
 //     const avatar2 = useSelector((state) => state.user?.profileData?.avatar.url);
 //     const authUsername = useSelector((state) => state.auth?.userData?.username);
 //     const dispatch = useDispatch();
@@ -373,6 +191,135 @@ export default TweetsFeed;
 
 
 
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { timeAgo } from "../helpers/timeAgo";
+import { Like, DeleteConfirmation, Edit } from "../components/index";
+import { HiOutlineDotsVertical } from "../components/icons";
+import { getAllTweets, deleteTweet, editTweet, likeTweet } from "../Store/Slices/tweetSlice";
+
+function TweetsFeed() {
+    const avatar2 = useSelector((state) => state.user?.profileData?.avatar.url);
+    const authUsername = useSelector((state) => state.auth?.userData?.username);
+    const tweets = useSelector((state) => state.tweet?.tweets);
+    const isLoading = useSelector((state) => state.tweet?.isLoading);
+    const dispatch = useDispatch();
+
+    const [editState, setEditState] = useState({
+        editing: false,
+        editedContent: "",
+        isOpen: false,
+        delete: false,
+        tweetId: null,
+    });
+
+    useEffect(() => {
+        dispatch(getAllTweets());
+    }, [dispatch]);
+
+    const handleEditTweet = (tweetId, editedContent) => {
+        dispatch(editTweet({ tweetId, content: editedContent }));
+        setEditState({ editing: false, isOpen: false, delete: false, tweetId: null });
+    };
+
+    const handleDeleteTweet = (tweetId) => {
+        dispatch(deleteTweet(tweetId));
+        setEditState({ editing: false, isOpen: false, delete: false, tweetId: null });
+    };
+
+    const handleLikeTweet = (tweetId) => {
+        dispatch(likeTweet(tweetId));
+    };
+
+    if (isLoading) {
+        return <div className="text-white text-center">Loading tweets...</div>;
+    }
+
+    return (
+        <div className="text-white w-full flex flex-col gap-3">
+            {tweets && tweets.length > 0 ? (
+                tweets.map((tweet) => (
+                    <div key={tweet._id} className="w-full flex sm:gap-5 gap-3 border-b border-slate-600 p-3 sm:p-5">
+                        <div className="w-10">
+                            <img
+                                src={tweet.owner?.avatar?.url || avatar2}
+                                className="w-8 h-8 object-cover rounded-full"
+                                alt="Avatar"
+                            />
+                        </div>
+                        <div className="w-full flex flex-col gap-1 relative">
+                            <div className="flex items-center gap-2">
+                                <h2 className="text-xs">{tweet.owner?.username}</h2>
+                                <span className="text-xs text-slate-400">{timeAgo(tweet.createdAt)}</span>
+                            </div>
+
+                            {editState.editing && editState.tweetId === tweet._id ? (
+                                <Edit
+                                    initialContent={editState.editedContent}
+                                    onCancel={() => setEditState({ editing: false, isOpen: false, tweetId: null })}
+                                    onSave={(editedContent) => handleEditTweet(tweet._id, editedContent)}
+                                />
+                            ) : (
+                                <p>{tweet.content}</p>
+                            )}
+
+                            <Like
+                                isLiked={tweet.isLiked}
+                                likesCount={tweet.likesCount}
+                                tweetId={tweet._id}
+                                size={20}
+                                onLike={() => handleLikeTweet(tweet._id)}
+                            />
+
+                            {authUsername === tweet.owner?.username && (
+                                <>
+                                    <div
+                                        className="w-5 h-5 absolute right-0 cursor-pointer"
+                                        onClick={() =>
+                                            setEditState({ isOpen: !editState.isOpen, tweetId: tweet._id, editedContent: tweet.content })
+                                        }
+                                    >
+                                        <HiOutlineDotsVertical />
+                                    </div>
+                                    {editState.isOpen && editState.tweetId === tweet._id && (
+                                        <div className="border bg-[#222222] text-lg border-slate-600 absolute right-5 rounded-xl">
+                                            <ul>
+                                                <li
+                                                    className="hover:opacity-50 px-5 cursor-pointer border-b border-slate-600"
+                                                    onClick={() => setEditState({ editing: true, isOpen: false })}
+                                                >
+                                                    Edit
+                                                </li>
+                                                <li
+                                                    className="px-5 hover:opacity-50 cursor-pointer"
+                                                    onClick={() => setEditState({ delete: true, isOpen: false })}
+                                                >
+                                                    Delete
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    )}
+                                </>
+                            )}
+
+                            {editState.delete && editState.tweetId === tweet._id && (
+                                <DeleteConfirmation
+                                    tweet={true}
+                                    onCancel={() => setEditState({ delete: false })}
+                                    onDelete={() => handleDeleteTweet(tweet._id)}
+                                />
+                            )}
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div className="text-white text-center">No tweets available.</div>
+            )}
+        </div>
+    );
+}
+
+export default TweetsFeed;
 
 
 
